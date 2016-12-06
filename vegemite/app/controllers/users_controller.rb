8e99e89397f27d1aa08106535b926b8a0840a71e
Wlavell2016 <william.lavell@gmail.com>
2016-12-06 15:50:50 -0500

    created user:model, controller methods

---
+++ vegemite/app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to gardens_url, notice: "signed up"
+        else
+          render "new"
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            redirect_to user_url(@user)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to gardens_url
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation)
+    end
+
+end
--- vegemite/db/schema.rb
+++ vegemite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206192630) do
+ActiveRecord::Schema.define(version: 20161206202617) do
 
   create_table "gardens", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20161206192630) do
     t.integer  "size"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
