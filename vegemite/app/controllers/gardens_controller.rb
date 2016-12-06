d11cc106d845f9b99320ebca2952fcd07d5dc4b0
Wlavell2016 <william.lavell@gmail.com>
2016-12-06 15:02:34 -0500

    generate gardens controller and methods

---
+++ vegemite/app/controllers/gardens_controller.rb
@@ -0,0 +1,47 @@
+class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+    @garden = Garden.find(params[:id])
+  end
+
+  def new
+      @garden = Garden.new
+  end
+
+  def create
+    @garden = Garden.new(garden_params)
+    if @garden.save
+      redirect_to garden_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @garden = Garden.find(params[:id])
+  end
+
+  def update
+    @garden = Garden.find(params[:id])
+    if @garden.update_attributes(garden_params)
+        redirect_to garden_url(@garden)
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    @garden = Garden.find(params[:id])
+    @garden.destroy
+    redirect_to gardens_url
+  end
+
+private
+
+  def garden_params
+    params.require(:garden).permit(:title, :description, :address, :size)
+  end
+end
