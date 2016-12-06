cbe9db378e3cdcd7951bf35cfd3b78031e390db5
Wlavell2016 <william.lavell@gmail.com>
2016-12-06 17:11:18 -0500

    added associations

--- vegemite/app/models/garden.rb
+++ vegemite/app/models/garden.rb
@@ -2,7 +2,9 @@ class Garden < ApplicationRecord
     belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
     # belongs_to :owner, source: User
     has_many :growers, class_name: 'User', foreign_key: 'user_id'
-    
+    #
+
+    # belongs_to :user
 
 
 end
--- vegemite/app/models/user.rb
+++ vegemite/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
     has_many :owned_gardens, class_name: 'Garden'
-
+    # has_many :gardens
 end
