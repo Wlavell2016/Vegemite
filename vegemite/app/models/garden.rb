0ed838191d647b10efffb7a12f8cce39d987992d
Wlavell2016 <william.lavell@gmail.com>
2016-12-06 19:39:29 -0500

    added new line

--- vegemite/app/models/garden.rb
+++ vegemite/app/models/garden.rb
@@ -1,10 +1,9 @@
 class Garden < ApplicationRecord
-    belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+    belongs_to :owner, class_name: 'User'
     # belongs_to :owner, source: User
-    has_many :growers, class_name: 'User', foreign_key: 'user_id'
-    #
+    has_many :growers, through: :reservations, source: :users, class_name: 'User'
+    has_many :reservations
 
+this is a test of our git hub
     # belongs_to :user
-
-
 end
