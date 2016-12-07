2b4a25858d0e098d181c57b703bb34202b5ce8aa
prisara <prisaramorale@gmail.com>
2016-12-07 17:41:00 -0500

    Add ensure log in and current user logic into application controller

--- vegemite/app/controllers/application_controller.rb
+++ vegemite/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ensure_logged_in
+    redirect_to root_url, alert: "Sorry, I can't let you do that." unless current_user
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # clarify whats goin on 
+  end
+
+  helper_method :current_user
+
 end
