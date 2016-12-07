72cd709e304d6950a6f7f2ed8550793088e9feeb
prisara <prisaramorale@gmail.com>
2016-12-06 19:30:58 -0500

    Add image column to users table

--- vegemite/db/schema.rb
+++ vegemite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207002547) do
+ActiveRecord::Schema.define(version: 20161207002913) do
 
   create_table "gardens", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161207002547) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image"
   end
 
   create_table "veggies", force: :cascade do |t|
