28394111ce6eb36b9915bf35c8d625f4ef07c012
prisara <prisaramorale@gmail.com>
2016-12-06 19:46:21 -0500

    Add associations to user and garden models

--- vegemite/app/models/garden.rb
+++ vegemite/app/models/garden.rb
@@ -3,7 +3,5 @@ class Garden < ApplicationRecord
     # belongs_to :owner, source: User
     has_many :growers, through: :reservations, source: :users, class_name: 'User'
     has_many :reservations
-
-this is a test of our git hub
     # belongs_to :user
 end
--- vegemite/app/models/user.rb
+++ vegemite/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+    has_many :reservations
     has_many :owned_gardens, source: :gardens, class_name: 'Garden'
-    # has_many :gardens
+    has_many :booked_gardens, through: :reservations
 end
