b9bc63c2b35048269804add2ddba5377df99920a
prisara <prisaramorale@gmail.com>
2016-12-08 22:09:25 -0500

    Add logic to sessions controller

--- vegemite/app/controllers/sessions_controller.rb
+++ vegemite/app/controllers/sessions_controller.rb
@@ -1,16 +1,24 @@
 class SessionsController < ApplicationController
-    def new
 
+    def new
     end
 
     def create
         @user = User.find_by_email(params[:email])
-        # should this be an instance  variable or a local variable
-        if @user && @user.authenticate
+        # should this be an instance variable or a local variable
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to root_url, notice: "Logged in!"
+        else
+          # render :new, alert: "Bad login"
+          flash.now[:alert] = "Invalid email or password"
+          render "new"
+        end
     end
 
     def destroy
+      session[:user_id] = nil
+      redirect_to gardens_url, notice: "Logged out!"
     end
 
-    
 end
