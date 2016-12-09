6e96816d0e6ee7e911cff3810f6257da4eb05384
prisara <prisaramorale@gmail.com>
2016-12-09 17:48:40 -0500

    Creating a reservation and garden are working, need to refine these more and add links

--- vegemite/app/controllers/reservations_controller.rb
+++ vegemite/app/controllers/reservations_controller.rb
@@ -10,18 +10,20 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
   end
 
-  # def new
-  #   @reservation = Reservation.new
-  #   @garden = Garden.find(params[:garden_id])
-  # end
+  def new
+    @reservation = Reservation.new
+    @garden = Garden.find(params[:garden_id])
+  end
 
   def create
+    # raise params.inspect
+    # binding.pry
     @garden = Garden.find(params[:garden_id])
     @reservation = @garden.reservations.build(reservation_params)
     @reservation.grower = current_user
     if @reservation.save
       # redirect_to garden_url(@garden), notice: 'Reservation created successfully'
-      redirect to garden_reservations_url, notice: 'Reservation created successfully'
+      redirect_to garden_reservations_url, notice: 'Reservation created successfully'
     else
       render 'gardens/show', alert: "Reservation couldn't be saved"
       # test the rendering output when the create method fails to save
@@ -29,22 +31,22 @@ class ReservationsController < ApplicationController
   end
 
   def edit
-        @reservation = Reservation.find(params[:id])
+    @reservation = Reservation.find(params[:id])
   end
 
   def update
-      @reservation = Reservation.find(params[:id])
-        if @reservation.update_attributes(reservation_params)
-            redirect_to garden_reservation_url(@reservation)
-            # create view or use console to make sure this route works
-            render :edit
-        end
+    @reservation = Reservation.find(params[:id])
+      if @reservation.update_attributes(reservation_params)
+        redirect_to garden_reservation_url(@reservation)
+        # create view or use console to make sure this route works
+        render :edit
+      end
   end
 
   def destroy
-      @reservation = Reservation.find(params[:id])
-      @reservation.destroy
-      redirect_to gardens_url
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to gardens_url
   end
 
   private
--- vegemite/app/models/reservation.rb
+++ vegemite/app/models/reservation.rb
@@ -1,9 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :garden
-  belongs_to :user
 
   belongs_to :grower, class_name: 'User'
-  belongs_to :owner, class_name: 'User'
+  has_one :owner, through: :garden, class_name: 'User'
 
   # has_one :owner, class_name: 'User', through : :garden
   # belongs_to :grower, class_name: 'User'
