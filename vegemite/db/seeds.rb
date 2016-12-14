be0a3c57872d121c399fa247d949a33647556d4d
nicky shieh <nicky.y.shieh@gmail.com>
2016-12-13 21:34:48 -0500

    Update seed file

--- vegemite/db/seeds.rb
+++ vegemite/db/seeds.rb
@@ -9,15 +9,30 @@
 require 'faker'
 
 20.times do
-  fullname = Faker::Name.name
-  firstname = fullname.split.first
+    fullname = Faker::Name.name
+    firstname = fullname.split.first
 
-  user = User.create!(
-    username: fullname.downcase,
-    firstname: firstname,
-    lastname: fullname.split.last,
-    email: "#{firstname}@#{Faker::Internet.domain_name}",
-    password: 'passpass',
-    password_confirmation: 'passpass'
-  )
+    user = User.create!(
+        username: fullname.downcase,
+        firstname: firstname,
+        lastname: fullname.split.last,
+        email: "#{Faker::Internet.free_email(firstname)}",
+        password: 'passpass',
+        password_confirmation: 'passpass'
+    )
+end
+
+20.times do
+    title = Faker::Pokemon.location
+    description = Faker::Lorem.paragraphs(1)
+    size = Faker::Number.number(3)
+    address = ['4576 connaught drive vancouver bc',
+               '1237 east broadway vancouver bc',
+               '3263 west 33rd avenue vancouver bc',
+               '1168 howe street vancouver bc']
+
+    garden = Garden.create!(title: title,
+                            description: description,
+                            address: address.sample,
+                            size: size)
 end
