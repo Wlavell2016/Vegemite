d961095486b52d4d6d4884bc4dca0cb175a6e495
prisara <prisaramorale@gmail.com>
2016-12-14 16:36:05 -0500

    Style image in user views

--- vegemite/app/controllers/users_controller.rb
+++ vegemite/app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation, :image)
   end
 
 end
