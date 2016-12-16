e8b4782311a72ff1fda992e278e719dbd522ff68
nicky shieh <nicky.y.shieh@gmail.com>
2016-12-16 11:04:56 -0500

    Edit views and controller for reservation

--- vegemite/app/controllers/reservations_controller.rb
+++ vegemite/app/controllers/reservations_controller.rb
@@ -11,14 +11,15 @@ class ReservationsController < ApplicationController
         @reservation = Reservation.find(params[:id])
     end
 
+
     def isoverlap
         @garden.reservations.each do |f|
             if f.startdate <= @reservation.enddate && @reservation.startdate <= f.enddate
                 return true
             end
         end
-        false
-    end
+                return false
+  end
 
     def new
         @reservation = Reservation.new
@@ -27,36 +28,45 @@ class ReservationsController < ApplicationController
     def create
         @garden = Garden.find(params[:garden_id])
         @reservation = @garden.reservations.build(reservation_params)
-        if isoverlap == false
+          if isoverlap == false
             @reservation.user = current_user
             if @reservation.save
                 redirect_to garden_reservation_url(@garden, @reservation), notice: 'Reservation made!'
             else
                 redirect_to garden_path(@garden), alert: @reservation.errors.full_messages
             end
-        else
-            redirect_to garden_reservations_path(@garden, @reservation), alert: 'Your Reservation overlaps with another reservation change your dates'
+          else
+         redirect_to garden_reservations_path(@garden, @reservation), alert: 'Your Reservation overlaps with another reservation change your dates'
         end
-  end
+   end
 
-    def edit
-        @garden = Garden.find(params[:garden_id])
-        @reservation = Reservation.find(params[:id])
-    end
 
-    def destroy
-        @reservation = Reservation.find(params[:id])
-        @reservation.destroy
-        redirect_to gardens_url
-    end
+        def update
+            @garden = Garden.find(params[:id])
+            @reservation = Reservation.find(params[:id])
+            if @reservation.update_attributes(reservation_params)
+                redirect_to garden_reservation_url(@reservation)
+            end
+         end
 
-    def load_garden
-        @garden = Garden.find(params[:garden_id])
-    end
+        def edit
+            @garden = Garden.find(params[:garden_id])
+            @reservation = Reservation.find(params[:id])
+        end
 
-    private
+        def destroy
+            @reservation = Reservation.find(params[:id])
+            @reservation.destroy
+            redirect_to gardens_url
+        end
 
-    def reservation_params
-        params.require(:reservation).permit(:note, :vegetable_id, :startdate, :enddate)
-    end
-end
+        def load_garden
+            @garden = Garden.find(params[:garden_id])
+        end
+
+        private
+
+        def reservation_params
+            params.require(:reservation).permit(:note, :vegetable_id, :startdate, :enddate)
+        end
+      end
