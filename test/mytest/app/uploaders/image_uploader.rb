a5fecac67916dc82cd29db08f686dcadcf1805d4
Wlavell2016 <william.lavell@gmail.com>
2017-01-05 12:03:26 -0500

    added image upload ability

--- test/mytest/app/uploaders/image_uploader.rb
+++ test/mytest/app/uploaders/image_uploader.rb
@@ -1,9 +1,10 @@
-class AvatarUploader < CarrierWave::Uploader::Base
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # changed above if not working test
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -24,28 +25,22 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  # process :scale => [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
-
-    version :banner do
-      process :resize_to_fill => [400, 400]
-    end
-  # changed above if not working test
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
-  # changed above if not working test
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
