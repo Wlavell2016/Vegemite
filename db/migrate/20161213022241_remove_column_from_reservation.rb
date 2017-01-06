42f8ad491a2be53a770bfa7eda0fd70782f34b06
PlainofthePo <nicky.y.shieh@gmail.com>
2017-01-06 16:12:39 -0500

    Change production db

--- db/migrate/20161213022241_remove_column_from_reservation.rb
+++ db/migrate/20161213022241_remove_column_from_reservation.rb
@@ -1,6 +1,6 @@
 class RemoveColumnFromReservation < ActiveRecord::Migration[5.0]
   def change
     remove_column :reservations, :vegetable_id, :integer
-    remove_column :users, :usertype, :string
+    # remove_column :users, :usertype, :string
   end
 end
