825b6eccc1343ff11ad5b0ab3308801e3122704f
Wlavell2016 <william.lavell@gmail.com>
2017-01-09 21:58:17 -0500

    pray to allah

--- db/migrate/20161207002547_add_image_to_gardens.rb
+++
@@ -1,7 +0,0 @@
-class AddImageToGardens < ActiveRecord::Migration[5.0]
-  def change
-    change_table :gardens do |t|
-      t.string :image
-    end
-  end
-end
--- db/migrate/20161213011701_change_column_in_gardens_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnInGardensTable < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :gardens, :owner_id, :user_id
-  end
-end
