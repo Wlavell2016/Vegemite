8d90090bdcb8016390aa07b3374e4864fd6a39a8
prisara <prisaramorale@gmail.com>
2017-01-12 02:02:30 -0500

    Fix indentations in garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,20 +1,21 @@
 class Garden < ApplicationRecord
-    belongs_to :user
-    has_many :reservations
-    has_many :users, through: :reservations
-    has_and_belongs_to_many :vegetables
+  belongs_to :user
+  has_many :reservations
+  has_many :users, through: :reservations
+  has_and_belongs_to_many :vegetables
 
-    def self.search(search)
-      where("title ILIKE ?", "%#{search}%")
-       .or(where("description ILIKE ?", "%#{search}%"))
-       .or(where("address ILIKE ?", "%#{search}%"))
-      #  .or(where("size ILIKE ?", "%#{search}%"))
-      #  .or(where("vegetable ILIKE ?", "%#{search}%"))
-    end
+  def self.search(search)
+    where("title ILIKE ?", "%#{search}%")
+     .or(where("description ILIKE ?", "%#{search}%"))
+     .or(where("address ILIKE ?", "%#{search}%"))
+    #  .or(where("size ILIKE ?", "%#{search}%"))
+    #  .or(where("vegetable ILIKE ?", "%#{search}%"))
+  end
 
-    mount_uploader :garden_image, GardenImageUploader
-    geocoded_by :address
-    after_validation :geocode, if: :address_changed?
-    # validates :title, :description, :address, :size, presence: true
-    # validates :size, numericality: {only_integer: true}
+  mount_uploader :garden_image, GardenImageUploader
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+  # validates :title, :description, :address, :size, presence: true
+  # validates :size, numericality: {only_integer: true}
+
 end
