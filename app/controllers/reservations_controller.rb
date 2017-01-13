d7be8e58d021071b938d4a21296a2fb2c01496a7
prisara <prisaramorale@gmail.com>
2017-01-13 01:50:13 -0500

    Tweak colours and content layout

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,71 +1,74 @@
 class ReservationsController < ApplicationController
    before_action :ensure_logged_in, only: [:create, :destroy]
    before_action :load_garden
+
  def new
      @reservation = Reservation.new
  end
 
-   def index
-       @reservations = Reservation.all
-   end
+  def index
+    @reservations = Reservation.all
+  end
 
-   def show
-       @garden = Garden.find(params[:garden_id])
-       @reservation = Reservation.find(params[:id])
-   end
+  def show
+    @garden = Garden.find(params[:garden_id])
+    @reservation = Reservation.find(params[:id])
+  end
 
-   def isoverlap(garden)
-       garden.reservations.each do |f|
-          if f.new_record?
-            next
-          end
-           if f.startdate <= @reservation.enddate && @reservation.startdate <= f.enddate
-               return true
-           end
-       end
-     return false
- end
+  def isoverlap(garden)
+    garden.reservations.each do |f|
+      if f.new_record?
+        next
+      end
+      if f.startdate <= @reservation.enddate && @reservation.startdate <= f.enddate
+        return true
+      end
+    end
+    return false
+  end
 
-   def create
-       @garden = Garden.find(params[:garden_id])
-       @reservation = @garden.reservations.build(reservation_params)
-         if isoverlap(@garden) == false
-           @reservation.user = current_user
-           if @reservation.save
-               redirect_to garden_reservation_url(@garden, @reservation), notice: 'Reservation made!'
-           else
-               redirect_to garden_path(@garden), alert: @reservation.errors.full_messages
-           end
-         else
-           redirect_to garden_reservations_path(@garden, @reservation), alert: 'Your Reservation overlaps with another reservation change your dates'
-       end
-   end
-
-       def update
-           @garden = Garden.find(params[:id])
-           @reservation = Reservation.find(params[:id])
-           if @reservation.update_attributes(reservation_params)
-               redirect_to garden_reservation_url(@reservation)
-           end
+  def create
+    @garden = Garden.find(params[:garden_id])
+    @reservation = @garden.reservations.build(reservation_params)
+      if isoverlap(@garden) == false
+        @reservation.user = current_user
+        if @reservation.save
+          redirect_to garden_reservation_url(@garden, @reservation), notice: 'Reservation made!'
+        else
+          redirect_to garden_path(@garden), alert: @reservation.errors.full_messages
         end
+      else
+        redirect_to garden_reservations_path(@garden, @reservation), alert: 'Your Reservation overlaps with another reservation change your dates'
+      end
+  end
 
-       def edit
-           @garden = Garden.find(params[:garden_id])
-           @reservation = Reservation.find(params[:id])
-       end
+  def update
+    @garden = Garden.find(params[:id])
+    @reservation = Reservation.find(params[:id])
+      if @reservation.update_attributes(reservation_params)
+        redirect_to garden_reservation_url(@reservation)
+      end
+  end
 
-       def destroy
-           @reservation = Reservation.find(params[:id])
-           @reservation.destroy
-           redirect_to gardens_url
-       end
+  def edit
+    @garden = Garden.find(params[:garden_id])
+    @reservation = Reservation.find(params[:id])
+  end
 
-       def load_garden
-           @garden = Garden.find(params[:garden_id])
-       end
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to gardens_url
+  end
 
-       private
-       def reservation_params
-           params.require(:reservation).permit(:note, :vegetable_id, :startdate, :enddate)
-       end
+  def load_garden
+    @garden = Garden.find(params[:garden_id])
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:note, :vegetable_id, :startdate, :enddate)
+  end
+
 end
