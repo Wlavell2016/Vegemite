b2635cdf96d97793ae14d4764daf12595aaf72dc
PlainofthePo <nicky.y.shieh@gmail.com>
2017-01-15 11:39:51 -0500

    Edit prod env

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Vegemite
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.enabled = true
   end
 end
