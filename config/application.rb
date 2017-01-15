5e5d353d7bee717282a2c8e58a03fba2ab5a92ce
PlainofthePo <nicky.y.shieh@gmail.com>
2017-01-15 02:50:31 -0500

    Config asset settings fo heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,14 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module Vegemite
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.enabled = true
   end
 end
